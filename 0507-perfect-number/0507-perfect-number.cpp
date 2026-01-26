cea8c0ffe9c280efc2248778a0c52d919c2ef0cb
Avinash Bharti <avinashbharti3007@gmail.com>
2026-01-26 11:19:09 +0530

    Time: 1059 ms (32.72%), Space: 7.9 MB (21.54%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -3,7 +3,7 @@ public:
     bool checkPerfectNumber(int num) {
         int curr_sum=0;
         int req_sum=num;
-        for(int i=1;i<num;i++){
+        for(int i=1;i*2<=num;i++){
             if(num%i==0) curr_sum += i;
         }
         if(curr_sum!=req_sum) return false;
